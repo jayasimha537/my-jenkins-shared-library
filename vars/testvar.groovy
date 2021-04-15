3b774ce3ae4b2ca72a601d154b01bf38d4745dd8
jayasimha537 <simha.ggd@gmail.com>
2021-04-15 07:51:36 +0530

    Update testvar.groovy

--- vars/testvar.groovy
+++ vars/testvar.groovy
@@ -1,9 +1,11 @@
 def call(map config){
+  def var1 = config.var1
   sh '''
   cat << EOF > pythonfile.py
   print ("""""""")
-  print ("var1" + config.var1)
+  print ("var1 : " + ${var1})
   print ("""""""")
   EOF
+  python pythonfile.py
   '''
 }
