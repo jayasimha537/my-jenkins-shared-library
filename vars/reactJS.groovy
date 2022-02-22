bad7950dfc0b95a91c8c29ee7003ee4a076bf7b8
jayasimha537 <simha.ggd@gmail.com>
2022-02-22 22:29:58 +0530

    Update reactJS.groovy

--- vars/reactJS.groovy
+++ vars/reactJS.groovy
@@ -14,6 +14,33 @@ def call(Map params){
           echo "Hai Jay"
         }
       }
+      stage("Param Condition"){
+        steps{
+        
+          if (params.CustomPath != null){
+            echo "CustomPath var is not null"
+            echo "Outside dir block -2 "
+            sh "pwd"
+            dir(params.CustomPath){
+              echo "Inside dir block"
+              sh '''
+                echo " Changed the working directory"
+                pwd
+                ls -ltrh
+              '''
+            }
+            echo "Outside dir block -2 "
+            sh "pwd"
+            
+          }
+          
+          else{
+            echo "CustomPath var is null"
+            sh "pwd"
+          }
+        
+        }
+      }
     }
     
   }
